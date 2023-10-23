7fa1119a522dac62b4c93ab644796adeeea3fbfb
BINET aurélie <au.binet21@gmail.com>
2023-10-23 20:17:03 +0200

    PRODUIT CONTROLLER

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -2,21 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
 use App\Form\ProductType;
+use PhpParser\Node\Name;
 use Symfony\Component\Routing\Annotation\Route;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class ProduitController extends AbstractController
 {
-    #[Route( '/' ,'Produit_index')]
-     public function index () ;
+    #[Route( '/Add',name:'Produit_index')]
+    public function AddProduit ()
     {
     $Produit = new Produit ;
-$form = $this->createForm(ProductType::class,$Produit);
-        return $this->render('Produit/add-Produit.html.twig');
+            $form = $this->createForm(ProductType::class,$Produit);
+                return $this->render('Produit/Add-Produit.html.twig',[
+                    'form' => $form->createView()
+
+                ]);
         
-        'form=>$form=>createView'()
     }
 }
 ?>
